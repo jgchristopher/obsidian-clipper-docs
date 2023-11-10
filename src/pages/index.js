011a0710b22ee3aca0b9984439c8872be050b95c
John Christopher <john@jdotc.xyz>
2023-11-10 14:25:01 -0600

    fix title and meta description

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,8 +39,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title}`}
+      description="Documentation site for the Obsidian Clipper plugin"
     >
       <HomepageHeader />
       <main></main>
